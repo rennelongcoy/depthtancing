03b93101b7141fb5ca7f6b19b91504843c16f0e3
Eli <renneruonkoi@gmail.com>
2021-02-17 23:29:25 +0900

    Replaced auto witth actual data types. Fixed formatting.

--- src/main.cpp
+++ src/main.cpp
@@ -6,46 +6,43 @@
 // include opencv library (Optional, used only for the following example)
 #include <opencv2/opencv.hpp>
 
-
 int main(){
-    using namespace std;
-
     // Create pipeline
     dai::Pipeline pipeline;
-    auto colorCam = pipeline.create<dai::node::ColorCamera>();
-    auto xlinkOut = pipeline.create<dai::node::XLinkOut>();
+    std::shared_ptr<dai::node::ColorCamera> colorCam = pipeline.create<dai::node::ColorCamera>();
+    std::shared_ptr<dai::node::XLinkOut> xlinkOut = pipeline.create<dai::node::XLinkOut>();
     xlinkOut->setStreamName("preview");
     colorCam->setInterleaved(true);
     colorCam->preview.link(xlinkOut->input);
-    
+    //colorCam->setPreviewSize(1280, 720);
+    //colorCam->setResolution(dai::ColorCameraProperties::SensorResolution::THE_1080_P);
 
     try {
         // Try connecting to device
         dai::Device device(pipeline);
 
         // Get output queue
-        auto preview = device.getOutputQueue("preview");
+        std::shared_ptr<dai::DataOutputQueue> preview = device.getOutputQueue("preview");
 
         // Start pipeline
         device.startPipeline();
 
-        cv::Mat frame;
         while (true) {
-
             // Receive 'preview' frame from device 
-            auto imgFrame = preview->get<dai::ImgFrame>();
+            std::shared_ptr<dai::ImgFrame> imgFrame = preview->get<dai::ImgFrame>();
 
             // Show the received 'preview' frame
-            cv::imshow("preview", cv::Mat(imgFrame->getHeight(), imgFrame->getWidth(), CV_8UC3, imgFrame->getData().data()));
+            cv::Mat frame(imgFrame->getHeight(), imgFrame->getWidth(), CV_8UC3, imgFrame->getData().data());
+            cv::imshow("preview", frame);
 
             // Wait and check if 'q' pressed
-            if (cv::waitKey(1) == 'q') return 0;
-
+            if (cv::waitKey(1) == 'q') {
+                return 0;
+            }
         }
     } catch (const std::runtime_error& err) {
         std::cout << err.what() << std::endl;
     }
-    
 
     return 0;
 }
