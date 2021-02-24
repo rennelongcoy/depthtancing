0d11fda0ce9c00ead37e7105dca798758e0961df
Eli <renneruonkoi@gmail.com>
2021-02-24 20:46:50 +0900

    Update CMakeLists.txt file for sample compile.

--- src/main.cpp
+++ src/main.cpp
@@ -2,49 +2,10 @@
 #include <iostream>
 #include <chrono>
 
-#include "utility.hpp"
+//#include "utility.hpp"
 
 // Inludes common necessary includes for development using depthai library
-#include "depthai/depthai.hpp"
-
-//static std::string label_map[] = {"background",  "aeroplane", "bicycle", "bird",      "boat",   "bottle",      "bus",   "car",  "cat",   "chair",    "cow",
-//                                  "diningtable", "dog",       "horse",   "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"};
-
-static std::string label_map[] = {"background", "person"};
-
-static bool syncNN = true;
-
-dai::Pipeline createNNPipeline(std::string nnPath) {
-    dai::Pipeline p;
-
-    auto colorCam = p.create<dai::node::ColorCamera>();
-    auto xlinkOut = p.create<dai::node::XLinkOut>();
-    auto detectionNetwork = p.create<dai::node::MobileNetDetectionNetwork>();
-    auto nnOut = p.create<dai::node::XLinkOut>();
-
-    xlinkOut->setStreamName("preview");
-    nnOut->setStreamName("detections");
-
-    //colorCam->setPreviewSize(300, 300);
-    colorCam->setPreviewSize(544, 320); // to match the mobilenet-ssd input size
-    colorCam->setResolution(dai::ColorCameraProperties::SensorResolution::THE_1080_P);
-    colorCam->setInterleaved(false);
-    colorCam->setColorOrder(dai::ColorCameraProperties::ColorOrder::BGR);
-    colorCam->setFps(40);
-
-    // testing MobileNet DetectionNetwork
-    detectionNetwork->setConfidenceThreshold(0.5f);
-    detectionNetwork->setBlobPath(nnPath);
-
-    // Link plugins CAM -> NN -> XLINK
-    colorCam->preview.link(detectionNetwork->input);
-    if(syncNN) detectionNetwork->passthrough.link(xlinkOut->input);
-    else colorCam->preview.link(xlinkOut->input);
-
-    detectionNetwork->out.link(nnOut->input);
-
-    return p;
-}
+#include <depthai/device.hpp>
 
 int main(int argc, char** argv) {
 
